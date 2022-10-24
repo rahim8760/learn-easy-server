fd891acf9593cbe7309f62aa720866a8ba399d14
md abdur rahim <rahim8760>
2022-10-25 05:08:45 +0600

    use cors

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const cors = require('cors');
 const app = express();
+const cors = require('cors');
 const port =process.env.PORT||5000;
-const courses=require('./data/courses.json')
+const courses=require('./data/courses.json');
+
 app.use(cors());
 app.get('/courses',(req, res)=>{
     res.send(courses);
