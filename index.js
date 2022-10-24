bd76f66df0b9544f6b6039748f0d98058fd15ae0
md abdur rahim <rahim8760>
2022-10-25 05:06:07 +0600

    add cors

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ const cors = require('cors');
 const app = express();
 const port =process.env.PORT||5000;
 const courses=require('./data/courses.json')
-
+app.use(cors());
+app.get('/courses',(req, res)=>{
+    res.send(courses);
+});
+app.get('/courses',(req, res)=>{
+    res.send(courses);
+});
 app.get('/',(req, res)=>{
     res.send('hello form server');
-})
+});
 app. listen(port, ()=>{console.log('this is learn easy server'),port})
